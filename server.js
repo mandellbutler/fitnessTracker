6c8ba9858bba7f12ca8d72937da33b52e842b573
Mandell Butler <mandell.butler@outlook.com>
2021-06-05 17:02:45 -0400

    Updated server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+// Add code to userModel.js to complete the model 18.1.13 REFERENCE
+
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 7540;
+
+const User = require("./userModel.js"); //make sure path is correct
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethoddb", { useNewUrlParser: true }); //change local host db name
+
+// Routes 
